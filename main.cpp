a1311164608dc5279c80a425d248eb0aa4de6b3a
Dennis Fiorenza <115047044+DenFio@users.noreply.github.com>
2022-11-17 18:21:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 
 int main() {
-   return 0;
-}
+   float a, b;
+   int opzione;
+
+   cin>>a>>b;
+   cin>>opzione;
+   
+   switch (opzione)  { 
+case 1: 
+   cout<<(a*b)/2;
+       break;
+case 2: 
+   cout<<a*a;
+       break;
+case 3: 
+   cout<<a*b;
+       break;
+   default:
+   
+   cout<<"Opzione non valida";
+  }
+   
+   
+   
+   
+  return 0;
+  }
